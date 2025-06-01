95b2268459252dbd55310521acbed17535486fb4
Đào Xuân Chiến <dxchien@gmail.com>
2025-06-01 13:58:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Facebook Ad Blocker
 // @namespace    http://tampermonkey.net/
-// @version      2.7
+// @version      2.8
 // @description  Xóa bài quảng cáo trên Facebook News Feed
 // @author       dxchien
 // @match        *://www.facebook.com/*
@@ -17,10 +17,19 @@
         let adsFound = false;
         const sponsoredText = "Được tài trợ".split('').filter(char => char.trim() !== '').map(char => `>${char}<`).join(' ');
 
+        // find svg id
+        var svgId;
+        const elements = document.querySelectorAll('text[id^="Svg"]');
+        elements.forEach(el => {
+            if(el.textContent.trim() == "Được tài trợ") {
+                svgId = "#" + el.id.trim();
+            }
+        });
+
         document.querySelectorAll('a[href]').forEach(link => {
             if (link.href.includes("?__cft__[0]=")) {
                 const linkHTML = link.innerHTML;
-                if (sponsoredText.split(' ').every(segment => linkHTML.includes(segment))) {
+                if (linkHTML.includes(svgId) || sponsoredText.split(' ').every(segment => linkHTML.includes(segment))) {
                     let parent = link;
                     for (let i = 0; i < 12; i++) {
                         if (parent.parentElement) {
